7762aabd6d80158add47c053ba88458df05431db
bhesha <77646813+bhesha@users.noreply.github.com>
2023-03-23 17:31:41 -0400

    Add files via upload

---
+++ graph1.C
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include "TGraphErrors.h"
+
+int graph1() {
+
+  // Define x and y data
+  const int nPoints = 10;
+  double x[nPoints] = {15, 28, 30, 33, 35, 47, 67, 108, 112, 137};
+  double y[nPoints] = {12, 14, 15, 23, 34, 55, 37,48, 34,56};
+  double yErr[nPoints] = {1.0, 2.0, 1.5, 2.5, 5.0, 11, 3.0 ,14.0 ,5.0 ,6.0 };  // Define errors for y values
+
+  // Create TGraphErrors object
+  TGraphErrors* gr = new TGraphErrors(nPoints, x, y, nullptr, yErr);
+
+  // Save data to CSV file
+  std::ofstream outFile("data2.csv");
+  outFile << "x,y,yErr" << std::endl;
+  for (int i = 0; i < nPoints; i++) {
+    outFile << x[i] << "," << y[i] << "," << yErr[i] << std::endl;
+  }
+  outFile.close();
+
+  // Plot graph
+  TCanvas* c1 = new TCanvas("c1", "Graph with Errors", 600, 400);
+  gr->SetTitle("Graph with Errors");
+  gr->GetXaxis()->SetTitle("x");
+  gr->GetYaxis()->SetTitle("y");
+  gr->SetMarkerStyle(20);
+  gr->SetMarkerSize(1.2);
+  gr->Draw("ALP");
+
+  return 0;
+}
