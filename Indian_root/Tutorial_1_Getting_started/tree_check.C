1ce60a61131dbcc6ca5abbfe1c58022b32e140e7
bhesha <77646813+bhesha@users.noreply.github.com>
2023-06-06 22:18:36 -0400

    different ways of checking trees of root file

--- Indian_root/Tutorial_1_Getting_started/tree_check.C
+++ Indian_root/Tutorial_1_Getting_started/tree_check.C
@@ -20,4 +20,26 @@ std::cout << "Tree " << treeName << " retrieved successfully." << std::endl;
 file->Close();
 delete file;
 }
+////////////////////////////////////////
+void tree_check(){
+TFile* file = new TFile("../../Flat_testout2.root", "READ");
+if (!file || file->IsZombie()) {
+  std::cout << "Error opening file: myFile.root" << std::endl;
+  return;
+}
+TString treeName = "rho0_Tree";
+TTree *tree = (TTree*)file->Get(treeName);
+
+if (!tree) {
+  std::cout << "Failed to retrieve tree " << treeName << "." << std::endl;
+  return;
+}
+
+std::cout << "Tree " << treeName << " retrieved successfully." << std::endl;
+// Rest of your code for working with the tree goes here
+
+// Clean up
+file->Close();
+delete file;
+}
 
